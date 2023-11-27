53b405f7a27c5ad317ea8199869d3da579ed4e4e
Diedrich Vorberg <diedrich@tux4web.de>
2023-11-27 13:00:24 +0100

    Started changes for pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- mode: python; encoding: utf-8; -*_
 
 ## This file is part of libtransliterate
@@ -19,36 +19,22 @@
 setup.py file for libtransliterate_unicode
 """
 
-import sys, os, os.path as op
 from distutils.core import setup, Extension
 
-usual_suspects = ( "/usr/local", "/usr", "/opt/local", "/sw", )
+name = "transliterate_betacode"
 
-base_dir = "/usr/local"
-for p in usual_suspects:
-    fp = op.join(p, "include", "transliterate.h")
-    if op.exists(fp):
-        base_dir = p
-        break
-
-include_dirs = [ op.join(base_dir, "include"), ]
-library_dirs = [ op.join(base_dir, "lib"), ]
-
-transliterate = Extension( "transliterate",
+transliterate = Extension( name,
                            sources=["betacode_coptic.cxx",
                                     "betacode_greek.cxx",
                                     "betacode_hebrew.cxx",
                                     "cjhebrew.cxx",
                                     "code_tables.cxx",
                                     "transliteratemodule.cxx",],
-                           include_dirs = include_dirs,
-                           library_dirs = library_dirs,
-                           libraries = ["transliterate",])
+                           libraries = [])
 
-setup (name = "transliterate",
-       version = "0.1",
+setup (name = name,
+       version = "0.2",
        author      = "Diedrich Vorberg <diedrich@tux4web.de>",
-       description = "Wrapper for libtransliterate that returns unicode " +\
-           "strings",
-       ext_modules = [transliterate],
-       py_modules = ["transliterate"], )
+       description = "Python module written in C++ to convert transliterated representation of ancient languages to unicode (TLG's beta code and LaTeX cjhebrew markup)",
+       ext_modules = [ transliterate ],
+       py_modules = [ name ], )
